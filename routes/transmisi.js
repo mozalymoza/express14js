0e7f394763e717d81886845e91069c93709038cb
mozalymoza <mozalymoza@example.com>
2023-10-10 16:23:09 +0700

    modul14

---
+++ routes/transmisi.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const router = express.Router();
+const {body, validationResult } = require('express-validator');
+const connection = require('../config/db');
+
+router.get('/', function (req, res) {
+    connection.query('select * from transmisi', function(err, rows){
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'Server Failed',
+            })
+        }else{
+            return res.status(200).json({
+                status:true,
+                message: 'Data transmisi',
+                data: rows
+            })
+        }
+    });
+});
+
+// untuk membuat data baru transmisi
+router.post('/store', [
+    body('nama_transmisi').notEmpty(),
+],(req, res) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(422).json({
+            error: error.array()
+        });
+    }
+    let Data = {
+        nama_transmisi: req.body.nama_transmisi,
+    }
+    connection.query('insert into transmisi set ?', Data, function(err, rows) {
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'Server Error'
+            })
+        }else{
+            return res.status(201).json({
+                status: true,
+                message: 'Success..!',
+                data: rows[0]
+            })
+        }
+    })
+})
+
+router.get('/(:id)', function (req, res) {
+    let id = req.params.id;
+    connection.query(`select * from transmisi where id_transmisi = ${id}`, function (err, rows) {
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'Server Error',
+            })
+        }
+        if(rows.lenght <=0){
+            return res.status(404).json({
+                status: false,
+                message: 'Not Found',
+            })
+        }
+        else{
+            return res.status(200).json({
+                status: true,
+                message: 'Data transmisi',
+                data: rows[0]
+            })
+        }
+    })
+})
+
+
+// untuk mengubah data transmisi
+router.patch('/update/:id', [
+    body('nama_transmisi').notEmpty(),
+
+], (req, res) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(422).json({
+            error: error.array()
+        });
+    }
+    let id = req.params.id;
+    let Data = {
+        nama_transmisi: req.body.nama_transmisi,
+    }
+    connection.query(`update transmisi set ? where id_transmisi = ${id}`, Data, function (err, rows) {
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'Server Error',
+            })
+        } else {
+            return res.status(200).json({
+                status: true,
+                message: 'Update Success..!',
+            })
+        }
+    })
+})  
+
+
+// untuk menghapus data transmisi
+router.delete('/delete/(:id)', function(req, res){
+    let id = req.params.id;
+    connection.query(`delete from transmisi where id_transmisi = ${id}`, function (err, rows) {
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'Server Error',
+            })
+        }else{
+            return res.status(200).json({
+                status: true,
+                message: 'Data has been delete !',
+            })
+        }
+    })
+})
+
+module.exports = router; // Corrected export statement
